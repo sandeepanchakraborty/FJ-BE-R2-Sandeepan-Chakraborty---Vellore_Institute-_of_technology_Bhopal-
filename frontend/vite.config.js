aa884d99bf59fe67a6cd50eb2cf89c9db5e9ecde
sandeepanchakraborty <sandeepanchakraborty123@gmail.com>
2026-02-26 01:17:12 +0530

    Add GitHub Pages deployment config and GitHub Actions workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FJ-BE-R2-Sandeepan-Chakraborty---Vellore_Institute-_of_technology_Bhopal-/',
 })
